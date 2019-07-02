7536ad3088566373422d96783b972b0b8fcd6173
Twdao <1424555751@qq.com>
2019-07-02 20:07:30 +0800

    newtext

--- src/text.java
+++ src/text.java
@@ -1,4 +1,8 @@
 public class text {
     String ni ="ss";
     String niha="niaao";
+    public void  main (String [] arg){
+        System.out.println(ni);
+        System.out.println(niha);
+    }
 }
